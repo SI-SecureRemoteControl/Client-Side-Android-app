defd19eae27576d8de4d030ef50e9835af04b589
eduvnjak2 <148522408+eduvnjak2@users.noreply.github.com>
2025-04-21 10:33:25 +0200

    Stop screen sharing fixed

--- app/src/main/java/ba/unsa/etf/si/secureremotecontrol/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/si/secureremotecontrol/MainActivity.kt
@@ -89,129 +89,6 @@ class MainActivity : ComponentActivity() {
                             },
                             onStartScreenCapture = { callback ->
                                 startScreenCapture(callback)
-                            }
-                        )
-                    }
-
-                    composable("deregister") {
-                        DeregistrationScreen(
-                            navController = navController,
-                            onDeregisterSuccess = {
-                                navController.navigate("registration") {
-                                    popUpTo("deregister") { inclusive = true }
-                                }
-                            }
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    private fun determineStartDestination(): String {
-        return runBlocking {
-            val token = tokenDataStore.token.first()
-            if (token.isNullOrEmpty()) "registration" else "main"
-        }
-    }
-
-    private fun startScreenCapture(callback: (resultCode: Int, data: Intent) -> Unit) {
-        val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
-        val captureIntent = mediaProjectionManager.createScreenCaptureIntent()
-        onScreenCaptureResult = callback
-        Log.d("MainActivity", "Starting screen capture with intent: $captureIntent")
-        screenCaptureLauncher.launch(captureIntent)
-    }
-
-
-
-}
-/*
-package ba.unsa.etf.si.secureremotecontrol
-
-import android.app.Activity
-import android.content.Intent
-import android.media.projection.MediaProjectionManager
-import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import ba.unsa.etf.si.secureremotecontrol.data.datastore.TokenDataStore
-import ba.unsa.etf.si.secureremotecontrol.presentation.main.MainViewModel
-import ba.unsa.etf.si.secureremotecontrol.presentation.verification.DeregistrationScreen
-import ba.unsa.etf.si.secureremotecontrol.ui.theme.SecureRemoteControlTheme
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
-import ba.unsa.etf.si.secureremotecontrol.service.ScreenSharingService
-
-@AndroidEntryPoint
-class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var tokenDataStore: TokenDataStore
-    private lateinit var screenCaptureLauncher: ActivityResultLauncher<Intent>
-
-    private var onScreenCaptureResult: ((resultCode: Int, data: Intent) -> Unit)? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Initialize the ActivityResultLauncher
-        screenCaptureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            val resultCode = result.resultCode
-            val data = result.data
-
-            Log.d("MainActivity", "Screen capture resultCode: $resultCode, data: $data")
-            if (resultCode == Activity.RESULT_OK && data != null) {
-                val fromId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
-                val intent = ScreenSharingService.getStartIntent(this, resultCode, data, fromId)
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    startForegroundService(intent) // For API 26 and above
-                } else {
-                    startService(intent) // For API 24 and 25
-                }
-            } else {
-                Log.e("MainActivity", "Screen capture permission denied or invalid data.")
-            }
-        }
-
-        setContent {
-            SecureRemoteControlTheme {
-                val navController = rememberNavController()
-                val viewModel: MainViewModel = hiltViewModel()
-
-                // Start observing RTC messages
-                viewModel.startObservingRtcMessages(this)
-
-                NavHost(
-                    navController = navController,
-                    startDestination = determineStartDestination()
-                ) {
-                    composable("registration") {
-                        RegisterScreen(
-                            onRegistrationSuccess = {
-                                navController.navigate("main") {
-                                    popUpTo("registration") { inclusive = true }
-                                }
-                            }
-                        )
-                    }
-
-                    composable("main") {
-                        MainScreen(
-                            onDeregister = {
-                                navController.navigate("deregister")
-                            },
-                            onStartScreenCapture = { callback ->
-                                startScreenCapture(callback)
                             },
                             onStopScreenCapture = {
                                 stopScreenCapture()
@@ -251,7 +128,10 @@ class MainActivity : ComponentActivity() {
 
     private fun stopScreenCapture() {
         val intent = ScreenSharingService.getStopIntent(this)
-        stopService(intent) // Stop the service
+        stopService(intent)
         Log.d("MainActivity", "Screen sharing stopped.")
     }
-}*/
+
+
+
+}
--- app/src/main/java/ba/unsa/etf/si/secureremotecontrol/MainScreen.kt
+++ app/src/main/java/ba/unsa/etf/si/secureremotecontrol/MainScreen.kt
@@ -31,7 +31,8 @@ import android.util.Log
 fun MainScreen(
     viewModel: MainViewModel = hiltViewModel(),
     onDeregister: () -> Unit,
-    onStartScreenCapture: (callback: (resultCode: Int, data: Intent) -> Unit) -> Unit
+    onStartScreenCapture: (callback: (resultCode: Int, data: Intent) -> Unit) -> Unit,
+    onStopScreenCapture: () -> Unit
 ) {
     val sessionState by viewModel.sessionState.collectAsState()
     var buttonEnabled by remember { mutableStateOf(true) }
@@ -117,173 +118,9 @@ fun MainScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = { viewModel.disconnectSession() }) {
-                Text("Disconnect")
-            }
-        } else {
-            Button(
-                onClick = {
-                    buttonEnabled = false
-                    viewModel.requestSession()
-                },
-                enabled = buttonEnabled
-            ) {
-                Text("Request Session")
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Button(
-                onClick = { viewModel.stopObservingMessages(); onDeregister() },
-                enabled = buttonEnabled
-            ) {
-                Text("Deregister Device")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        when (sessionState) {
-            is SessionState.Idle -> {
-                buttonEnabled = true
-            }
-            is SessionState.Requesting -> Text("Requesting session...")
-            is SessionState.Timeout -> {
-                Text("Session request timed out.")
-                buttonEnabled = true
-            }
-            is SessionState.Accepted -> Text("Session accepted!")
-            is SessionState.Waiting -> Text("Waiting for response...")
-            is SessionState.Rejected -> {
-                Text("Session rejected.")
-                buttonEnabled = true
-            }
-            is SessionState.Error -> {
-                val errorMessage = (sessionState as SessionState.Error).message
-                Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
-                buttonEnabled = true
-                viewModel.resetSessionState()
-            }
-            else -> {}
-        }
-    }
-}
-/*
-package ba.unsa.etf.si.secureremotecontrol
-
-import android.content.Intent
-import android.provider.Settings
-import android.util.Log
-import android.widget.Toast
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import ba.unsa.etf.si.secureremotecontrol.presentation.main.MainViewModel
-import ba.unsa.etf.si.secureremotecontrol.presentation.main.SessionState
-
-@Composable
-fun MainScreen(
-    viewModel: MainViewModel = hiltViewModel(),
-    onDeregister: () -> Unit,
-    onStartScreenCapture: (callback: (resultCode: Int, data: Intent) -> Unit) -> Unit,
-    onStopScreenCapture: () -> Unit // Added parameter for stopping screen sharing
-) {
-    val sessionState by viewModel.sessionState.collectAsState()
-    var buttonEnabled by remember { mutableStateOf(true) }
-
-    val context = LocalContext.current
-
-    when (sessionState) {
-        is SessionState.Rejected -> {
-            Toast.makeText(context, "Session request rejected.", Toast.LENGTH_LONG).show()
-            viewModel.resetSessionState()
-        }
-        is SessionState.Accepted -> {
-            var showDialog by remember { mutableStateOf(true) }
-
-            if (showDialog) {
-                AlertDialog(
-                    onDismissRequest = { showDialog = false },
-                    title = { Text("Confirm Session") },
-                    text = { Text("Do you want to confirm the session?") },
-                    confirmButton = {
-                        Button(onClick = {
-                            showDialog = false
-                            viewModel.sendSessionFinalConfirmation(true) // User accepted
-                            onStartScreenCapture { resultCode, data ->
-                                val fromId = Settings.Secure.getString(
-                                    context.contentResolver,
-                                    Settings.Secure.ANDROID_ID
-                                )
-                                Log.d("MainScreen", "MainScreen je problem: $resultCode, data: $data, fromId: $fromId")
-                                viewModel.startStreaming(resultCode, data, fromId)
-                            }
-                        }) {
-                            Text("Yes")
-                        }
-                    },
-                    dismissButton = {
-                        Button(onClick = {
-                            showDialog = false
-                            viewModel.sendSessionFinalConfirmation(false) // User rejected
-                        }) {
-                            Text("No")
-                        }
-                    }
-                )
-            }
-        }
-        else -> {}
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Image(
-            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
-            contentDescription = stringResource(id = R.string.app_name),
-            modifier = Modifier
-                .height(180.dp)
-                .graphicsLayer(
-                    scaleX = 2.5f,
-                    scaleY = 2.5f,
-                    translationX = 10f
-                )
-        )
-
-        Text(
-            textAlign = TextAlign.Center,
-            text = stringResource(id = R.string.title),
-            style = MaterialTheme.typography.headlineMedium,
-            color = MaterialTheme.colorScheme.primary,
-            fontWeight = FontWeight.Bold,
-        )
-
-        Spacer(modifier = Modifier.height(64.dp))
-
-        if (sessionState is SessionState.Connected) {
-            Text("Connected", style = MaterialTheme.typography.bodyLarge)
-
-            Spacer(modifier = Modifier.height(16.dp))
-
             Button(onClick = {
-                viewModel.disconnectSession()
-                onStopScreenCapture() // Stop screen sharing when disconnecting
+                viewModel.disconnectSession() // Disconnect the session
+                onStopScreenCapture() // Stop screen sharing
             }) {
                 Text("Disconnect")
             }
@@ -334,4 +171,4 @@ fun MainScreen(
             else -> {}
         }
     }
-}*/
+}
